1ceae5822c5568d178632cfb68b79601921ace32
Adrian Martinez <adrian.martinez@scholar.smashprogram.org>
2018-06-22 10:08:35 -0700

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("[Your Name Here]"); // enter your name here!
+  myName.html("Adrian"); // enter your name here!
 }
 
  /*
@@ -21,15 +21,18 @@ function setup() {
 
 // Write all your code inside the draw() function below!
 function draw() {
-    var img;
-    function preLoad() {
-        img loadImage("download.jpg");
-    }
-    function setup(){
-        Image(img, 120, 120);
+background('black'); // try changing this to a blue background
+  fill('blue');    // Always call fill() before the shape you want to fill in.
+  rect(60, 70, 680, 440); // Draw a 30x20 pixel rectangle at coordinate (55,55)
+    line(390,190, 390, 440);
+    triangle(130, 200, 150, 120, 275, 300);
+    triangle(530, 200, 550, 120, 675, 300);
+    arc(400, 450, 80, 80, 0, PI + QUARTER_PI, OPEN);
+    fill("red");
+    ellipse(230, 490, 210, 135);
+    
+    console.log("I am below line function");
+    
+    drawthreelittlecircles();
     }
   // Example. You can remove when you're ready!
-  background(0, 255, 0); // try changing this to a blue background
-  fill(255, 204, 0);    // Always call fill() before the shape you want to fill in.
-  rect(30, 20, 55, 55); // Draw a 30x20 pixel rectangle at coordinate (55,55)
-}
